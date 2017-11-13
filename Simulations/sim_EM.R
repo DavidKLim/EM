30ab1c2d359b1238df6130dea46692f50ba80d8f
David <deelim@live.unc.edu>
2017-11-13 13:31:40 -0500

    Fixed simulations to save datasets, and to filter out genes with <= 100 count (to address excessive 0's). Also, basing simulations on NSCLC data repeated 5 times (to add n)

--- Simulations/sim_EM.R
+++ Simulations/sim_EM.R
@@ -4,6 +4,8 @@ init_y<-read.table("init_y.txt")
 #init_size_factors<-as.numeric(read.table("init_size_factors.txt")[,1])
 #init_norm_y<-read.table("init_norm_y.txt")
 
+init_y<-cbind(init_y,init_y,init_y,init_y,init_y)
+
 
 library("stats")
 library("data.table")
@@ -91,7 +93,7 @@ simulate_data_g=function(n,k,g,init_pi,b,size_factors,method,phi=rep(0,times=g))
 
 # Function to perform EM on simulated data
 sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
-  # max n = 100, max g = 1088 w/ current (CancerSubtypes GeneExp data)#
+  # max n = 100, max #
   
   if(method=="poisson"){
     source("Pan EM.R")
@@ -161,6 +163,8 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
   sim=100
   choose_k<-rep(0,times=sim)
   
+  all_data <- list(list())
+  
   for(ii in 1:sim){
     
     # Simulate data based on initial estimates/estimate size factors
@@ -178,14 +182,38 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
     for(i in 1:n){
       norm_y[,i] = y[,i]/size_factors[i]
     }
+    true_disc=c(rep(TRUE,tt),rep(FALSE,(g-tt)))
     
-    # Simulation order selection based on unpenalized EM #
+    idx <- rowSums(y)>100      # mark only genes with >100 count total: take out genes with excess 0's or too low count
     
-    K_search=c(2:7)
-    list_BIC=matrix(0,nrow=length(K_search),ncol=2)
-    list_BIC[,1]=K_search
+    y <- y[idx,]
+    norm_y <- norm_y[idx,]
+    true_disc <- true_disc[idx]
+    
+    all_data[[ii]]<-list(y=y,
+                         true_clusters=true_clusters,
+                         norm_y=norm_y,
+                         true_disc=true_disc)
+  }
+    
+  
+  
+  
+  
+  
+  
+  # Simulation order selection based on unpenalized EM #
+  
+  K_search=c(2:7)
+  list_BIC=matrix(0,nrow=length(K_search),ncol=2)
+  list_BIC[,1]=K_search
+  for(ii in 1:sim){
     
     print(paste("Iteration",ii,":"))
+    y = all_data[[ii]]$y
+    true_clusters = all_data[[ii]]$true_clusters
+    norm_y = all_data[[ii]]$norm_y
+    
     for(aa in 1:nrow(list_BIC)){
       list_BIC[aa,2]<-EM(y=y,k=list_BIC[aa,1],lambda1=0,lambda2=0,tau=0,size_factors=size_factors,norm_y=norm_y,true_clusters=true_clusters)$BIC  # no penalty
       print(list_BIC[aa,])
@@ -209,7 +237,6 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
   
   # Simulations to do Grid search for tuning parameters
   
-  sim=10
   choose_lambda1<-rep(0,times=sim)
   choose_lambda2<-rep(0,times=sim)
   choose_tau<-rep(0,times=sim)
@@ -219,22 +246,14 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
     print(paste("Iteration",ii,"of grid search"))    # track iteration
     
     #simulate data
-    sim.dat<-simulate_data(n=n,k=true.K,g=g,init_pi=sim_pi,b=sim_coefs,size_factors=size_factors,method=method)
-    y<-sim.dat$y
-    z<-sim.dat$z
-    true_clusters<-rep(0,times=n)
-    for(i in 1:n){
-      true_clusters[i]<-which(z[,i]==1)
-    }
-    norm_y = y
-    for(i in 1:n){
-      norm_y[,i] = y[,i]/size_factors[i]
-    }
+    y = all_data[[ii]]$y
+    true_clusters = all_data[[ii]]$true_clusters
+    norm_y = all_data[[ii]]$norm_y
     
     #create matrix for grid search values
     lambda1_search=1
-    lambda2_search=seq(from=0.1,to=2,by=0.1)
-    tau_search=seq(from=0.1,to=2,by=0.1)
+    lambda2_search=seq(from=0.1,to=1,by=0.1)
+    tau_search=seq(from=0.1,to=1,by=0.1)
     
     list_BIC=matrix(0,nrow=length(lambda1_search)*length(lambda2_search)*length(tau_search),ncol=4) # matrix of BIC's: one for each combination of penalty params 
     
@@ -269,6 +288,7 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
     }
   }
   
+  
   #store max penalty parameters based on multiple simulations
   tab_tau<-table(choose_tau)
   tab_lambda1<-table(choose_lambda1)
@@ -291,46 +311,38 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
   
   
   # Simulations for determining performance based on optimal K and penalty params
-  sim=100
   temp_pi<-matrix(rep(0,times=k*sim),nrow=sim)
-  temp_coefs<-list()
+  #temp_coefs<-list()
   temp_nondisc<-rep(0,times=sim)
   temp_ARI<-rep(0,times=sim)
   temp_sensitivity<-rep(0,times=sim)
   temp_falsepos<-rep(0,times=sim)
   for(ii in 1:sim){
-    sim.dat<-simulate_data(n=n,k=max_k,g=g,init_pi=sim_pi,b=sim_coefs,size_factors=size_factors,method=method)
-    y<-sim.dat$y
-    z<-sim.dat$z
-    true_clusters<-rep(0,times=n)
-    for(i in 1:n){
-      true_clusters[i]<-which(z[,i]==1)
-    }
     
-    norm_y = y
-    for(i in 1:n){
-      norm_y[,i] = y[,i]/size_factors[i]
-    }
+    y = all_data[[ii]]$y
+    true_clusters = all_data[[ii]]$true_clusters
+    norm_y = all_data[[ii]]$norm_y
+    true_disc = all_data[[ii]]$true_disc
     
     
     # sometimes errors if wrong K is input
     X<-EM(y=y,k=k,tau=max_tau,lambda1=max_lambda1,lambda2=max_lambda2,size_factors=size_factors,norm_y=norm_y,true_clusters=true_clusters)
     temp_pi[ii,]<-X$pi
-    temp_coefs[[ii]]<-X$coefs
+    #temp_coefs[[ii]]<-X$coefs
     temp_nondisc[ii]<-mean(X$nondiscriminatory)
     temp_ARI[ii]<-adjustedRandIndex(true_clusters,X$final_clusters)
     if(tt>0){
-      temp_sensitivity[ii]<-mean(X$nondiscriminatory[1:tt]==FALSE)
+      temp_sensitivity[ii]<-mean(X$nondiscriminatory[true_disc==TRUE]==FALSE)
     } else {temp_sensitivity[ii]<-NA}
     if(tt<g){
-      temp_falsepos[ii]<-mean(X$nondiscriminatory[(tt+1):g]==FALSE)
+      temp_falsepos[ii]<-mean(X$nondiscriminatory[true_disc==FALSE]==FALSE)
     } else {temp_falsepos[ii]<-NA}
     
     print(paste(temp_nondisc[ii],temp_ARI[ii],temp_sensitivity[ii],temp_falsepos[ii]))
   }
   
   mean_pi<-colSums(temp_pi)/sim
-  mean_coefs<-Reduce('+',temp_coefs)/sim
+  #mean_coefs<-Reduce('+',temp_coefs)/sim
   mean_nondisc<-mean(temp_nondisc)
   mean_ARI<-mean(temp_ARI)
   mean_sensitivity<-mean(temp_sensitivity)
@@ -345,6 +357,7 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
                 ARI=mean_ARI,
                 nondisc=mean_nondisc,
                 sens=mean_sensitivity,
-                falsepos=mean_falsepos)
+                falsepos=mean_falsepos,
+                all_data)
   return(results)
 }
