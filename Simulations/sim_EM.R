174ffa424eac8108e275381913da62ee51201041
David <deelim@live.unc.edu>
2017-11-21 15:00:11 -0500

    fixed sim/10

--- Simulations/sim_EM.R
+++ Simulations/sim_EM.R
@@ -246,11 +246,11 @@ sim.EM<-function(true.K, fold.change, num.disc, g, n, method){
   
   # Simulations to do Grid search for tuning parameters
   
-  choose_lambda1<-rep(0,times=sim)
-  choose_lambda2<-rep(0,times=sim)
-  choose_tau<-rep(0,times=sim)
+  choose_lambda1<-rep(0,times=sim/10)
+  choose_lambda2<-rep(0,times=sim/10)
+  choose_tau<-rep(0,times=sim/10)
   
-  for(ii in 1:sim/10){
+  for(ii in (1:sim)/10){
     
     print(paste("Iteration",ii,"of grid search"))    # track iteration
     
