ff62df7d3e851c76f20992a9642fba440a8e5391
David Lim <limddavid@Davids-MacBook-Air.local>
2017-12-07 22:27:14 -0500

    Comparisons between iCluster+ and my method

--- comparisons.R
+++ comparisons.R
@@ -1,4 +1,199 @@
-source("https://www.bioconductor.org/biocLite.R")
-biocLite("iClusterPlus")
 
-library("iClusterPlus")
+library(iClusterPlus)
+library(DESeq2)
+
+
+# DESeq analysis
+normalizations <- function(dat){
+  n=ncol(dat)
+  g=nrow(dat)
+  row_names<-paste("gene",seq(g))
+  col_names<-paste("subj",seq(n))
+  cts<-round(as.matrix(dat),digits=0)
+  rownames(cts)<-row_names
+  colnames(cts)<-col_names
+  coldata<-data.frame(matrix(paste("cl"),nrow=n))
+  rownames(coldata)<-colnames(cts)
+  colnames(coldata)<-"cluster"
+  dds<-DESeqDataSetFromMatrix(countData = cts,
+                              colData = coldata,
+                              design = ~ 1)
+  DESeq_dds<-DESeq(dds)
+  size_factors<-sizeFactors(DESeq_dds)
+  norm_y<-counts(DESeq_dds,normalized=TRUE)
+  
+  res <- list(size_factors=size_factors,
+              norm_y=norm_y)
+  return(res)
+}
+
+# top 100 significant genes
+# res<-results(DESeq_dds,alpha=0.05)
+# signif_res<-res[is.na(res$padj)==FALSE,]
+# signif_res<-signif_res[order(signif_res$padj),]
+# signif_res<-signif_res[1:100,]
+# 
+# rownames(dat)<-row_names
+# signif_dat<-dat[toupper(rownames(signif_res)),]      # Subsetting just the significant genes from DESeq2
+# y<-signif_dat
+
+#rownames(dat)=toupper(row_names)
+
+
+compare <- function(y){
+  
+  # list of K to search over
+  K_search = c(2:7)
+
+  
+  # iClusterPlus #
+  i_tune_start_time <- Sys.time()
+  
+  iClust_OS <- list()
+  for(k in (K_search-1)){
+    cv.fit = tune.iClusterPlus(cpus=1,dt1=t(y),type="poisson",K=k,alpha=1,n.lambda=25,scale.lambda=1,maxiter=20)
+    iClust_OS[[k]] = cv.fit
+  }
+  BIC_mat = getBIC(iClust_OS)
+  
+  BIC_OS = apply(BIC_mat,2,min)
+  lambda_OS = iClust_OS[[1]]$lambda[apply(BIC_mat,2,which.min),1]
+  
+  i_max_k = which.min(BIC_OS)
+  max_lambda = lambda_OS[i_max_k]
+  
+  i_tune_end_time <- Sys.time()
+  i_tune_time <- as.numeric(i_tune_end_time)-as.numeric(i_tune_start_time)
+  
+  i_start <- Sys.time()
+  iClust_fit <- iClusterPlus(dt1=t(y),type="poisson",lambda=max_lambda,alpha=1,K=i_max_k,maxiter=10)   # K=1 is true
+  i_end <- Sys.time()
+  i_time <- as.numeric(i_end)-as.numeric(i_start)
+  
+  icoefs <- cbind(iClust_fit$alpha[[1]],iClust_fit$alpha[[1]]+iClust_fit$beta[[1]])
+  
+  
+  features = rownames(y)
+  
+  rowsum=apply(abs(iClust_fit$beta[[1]]),1, sum)
+  upper=quantile(rowsum,prob=0.75)
+  isigfeatures=(features)[which(rowsum>upper)]
+  
+  print(paste("Order:",(i_max_k+1)))
+  print(paste("Clusters:",iClust_fit$clusters))
+  
+  
+  
+  
+  # my method #
+  source("/Users/limddavid/Documents/Research/NB Pan EM par.R")
+  
+  EM_tune_start <- Sys.time()
+  
+  list_BIC=matrix(0,nrow=length(K_search),ncol=2)
+  list_BIC[,1]=K_search
+  
+  for(aa in 1:nrow(list_BIC)){
+    list_BIC[aa,2]<-EM(y=y,k=list_BIC[aa,1],lambda1=0,lambda2=0,tau=0,size_factors=size_factors,norm_y=norm_y)$BIC   # no penalty Pan
+    print(list_BIC[aa,])
+  }
+  
+  max_k=list_BIC[which(list_BIC[,2]==min(list_BIC[,2])),1]
+  
+  lambda1_search=1
+  lambda2_search=c(0.1,0.15,0.2,0.5,0.9)
+  tau_search=seq(from=0.1,to=0.9,by=0.2)
+  
+  list_BIC=matrix(0,nrow=length(lambda1_search)*length(lambda2_search)*length(tau_search),ncol=4) # matrix of BIC's: one for each combination of penalty params 
+  
+  list_BIC[,1]=rep(lambda1_search,each=length(lambda2_search)*length(tau_search))
+  list_BIC[,2]=rep(rep(lambda2_search,each=length(tau_search)),times=length(lambda1_search))
+  list_BIC[,3]=rep(tau_search,times=length(lambda1_search)*length(lambda2_search))
+  
+  #search for optimal penalty parameters
+  for(aa in 1:nrow(list_BIC)){
+    X<-EM(y=y,k=max_k,tau=list_BIC[aa,3],lambda1=list_BIC[aa,1],lambda2=list_BIC[aa,2],size_factors=size_factors,norm_y=norm_y,true_clusters=NA)
+    list_BIC[aa,4]<-X$BIC
+    print(list_BIC[aa,])
+    print(paste("Time:",X$time_elap,"seconds"))
+  }
+  
+  max_index<-which(list_BIC[,4]==min(list_BIC[,4]))
+  max_tau<-list_BIC[max_index,3]
+  max_lambda1<-list_BIC[max_index,1]
+  max_lambda2<-list_BIC[max_index,2]
+  
+  print(list_BIC[max_index,])
+  
+  if(length(max_index)>1){
+    warning("more than one max index")
+    max_index<-max_index[1]
+    max_tau<-list_BIC[max_index,3]
+    max_lambda1<-list_BIC[max_index,1]
+    max_lambda2<-list_BIC[max_index,2]
+  }
+  
+  EM_tune_end <- Sys.time()
+  
+  EM_tune_time <- as.numeric(EM_tune_end)-as.numeric(EM_tune_start)
+  
+  EM_start <- Sys.time()
+  X<-EM(y=y,k=max_k,tau=max_tau,lambda1=max_lambda1,lambda2=max_lambda2,size_factors=size_factors,norm_y=norm_y,true_clusters=NA)
+  EM_end <- Sys.time()
+  EM_time <- as.numeric(EM_end)-as.numeric(EM_start)
+  
+  EM_sigfeatures<-features[X$nondiscriminatory==FALSE]
+  
+  list(i_maxK=(i_max_k+1),
+       EM_maxK=max_k,
+       i_sigfeatures=isigfeatures,
+       EM_sigfeatures=EM_sigfeatures,
+       i_clusters=iClust_fit$clusters,
+       EM_clusters=X$final_clusters,
+       i_tune_time=i_tune_time,
+       i_time=i_time,
+       EM_tune_time=EM_tune_time,
+       EM_time=EM_time)
+}
+
+setwd("/Users/limddavid/Documents/Research")
+
+NSCLC_anno <- read.table("Real Data/Lung Cancer Cell Line/NSCLC_anno.txt",sep="\t",header=TRUE)
+NSCLC_dat <- read.table("Real Data/Lung Cancer Cell Line/NSCLC_rsem.genes.exp.count.unnormalized.txt",sep="\t",header=TRUE)
+rownames(NSCLC_dat)<-toupper(NSCLC_dat[,1])
+NSCLC_subtypes <- NSCLC_anno$Adeno.Squamous
+NSCLC_dat<-round(NSCLC_dat[,-1],digits=0)
+
+library(SummarizedExperiment)
+
+load(file="TCGA_BRCA_exp.rda")
+BRCA_cts <- round(assay(data),digits=0)             # default gives raw count
+BRCA_anno <- colData(data)@listData
+BRCA_subtypes <- BRCA_anno$subtype_PAM50.mRNA
+idy <- !is.na(BRCA_subtypes)
+
+BRCA_dat <- BRCA_cts[, idy]           # 500 subjects
+BRCA_dat <- distinct(as.data.frame(BRCA_dat))
+
+# NSCLC first 2000 genes run #
+y1 <- NSCLC_dat[1:2000,]
+fit <- normalizations(y1)
+size_factors <- fit$size_factors
+norm_y <- fit$norm_y
+
+y1 <- y1[rowSums(y1) >= 100,]
+
+X1<-compare(y1)
+
+
+# BRCA first 2000 genes, 100 samples run #
+
+BRCA_dat <- BRCA_dat[order(-rowVars(BRCA_dat)),]
+y2 <- BRCA_dat[1:500,1:50]
+fit <- normalizations(y2)
+size_factors <- fit$size_factors
+norm_y <- fit$norm_y
+
+y2 <- y2[rowSums(y2 >= 100),]
+
+X2 <- compare(y2)
