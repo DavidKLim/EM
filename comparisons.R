5a183b86bb7ab8b84d81882371525a33b7732d92
KYUNG T LIM <deelim@KYUNGs-MacBook-Air.local>
2018-01-16 16:24:12 -0500

    added function to create CSV summary table

--- comparisons.R
+++ comparisons.R
@@ -76,7 +76,7 @@ compare <- function(y){
   
   
   # my method #
-  source("/Users/limddavid/Documents/Research/NB Pan EM par.R")
+  source("/Users/deelim/Documents/Research/NB Pan EM par.R")
   
   EM_tune_start <- Sys.time()
   
@@ -148,7 +148,11 @@ compare <- function(y){
        EM_X=X)
 }
 
-setwd("/Users/limddavid/Documents/Research")
+setwd("/Users/deelim/Documents/Research")
+
+################################ NSCLC (Cell Line) Data #######################################
+
+# DATA PREP
 
 NSCLC_anno <- read.table("Real Data/Lung Cancer Cell Line/NSCLC_anno.txt",sep="\t",header=TRUE)
 NSCLC_dat <- read.table("Real Data/Lung Cancer Cell Line/NSCLC_rsem.genes.exp.count.unnormalized.txt",sep="\t",header=TRUE)
@@ -156,69 +160,30 @@ rownames(NSCLC_dat)<-toupper(NSCLC_dat[,1])
 NSCLC_subtypes <- NSCLC_anno$Adeno.Squamous
 NSCLC_dat<-round(NSCLC_dat[,-1],digits=0)
 
-# DESeq2 to find top significant genes in clustering
+
+# PRE-FILTERING
+
+## DESeq2 to find top significant genes in clustering
 colnames(NSCLC_dat)<-toupper(colnames(NSCLC_dat))
 coldata<-NSCLC_anno[,-1]
 rownames(coldata)<-toupper(NSCLC_anno[,1])
 coldata<-coldata[,c("Adeno.Squamous","Tumor.location")]
-#all(rownames(coldata) %in% colnames(cts))         # check that headers are correct
-#all(rownames(coldata) == colnames(cts))
 dds<-DESeqDataSetFromMatrix(countData = NSCLC_dat,
                             colData = coldata,
                             design = ~ Adeno.Squamous)
 DESeq_dds<-DESeq(dds)
-
 res<-results(DESeq_dds,alpha=0.05)
 signif_res<-res[is.na(res$padj)==FALSE,]
 signif_res<-signif_res[order(signif_res$padj),]
-signif_res<-signif_res[1:1000,]
-
+signif_res<-signif_res[1:100,]
 NSCLC_dat <- NSCLC_dat[rownames(signif_res),]
 
-
-
-library(SummarizedExperiment)
-library(genefilter)
-
-load(file="TCGA_BRCA_exp.rda")
-BRCA_cts <- round(assay(data),digits=0)             # default gives raw count
-BRCA_anno <- colData(data)@listData
-BRCA_subtypes <- BRCA_anno$subtype_PAM50.mRNA
-#idy <- c(which(BRCA_subtypes=="Basal-like")[1:10],
-#         which(BRCA_subtypes=="HER2-enriched")[1:10],
-#         which(BRCA_subtypes=="Luminal A")[1:10],
-#         which(BRCA_subtypes=="Luminal B")[1:10],
-#         which(BRCA_subtypes=="Normal-like")[1:8])
-idy <- (!is.na(BRCA_subtypes) & BRCA_subtypes != "Normal-like")
-
-#BRCA_dat <- unique(as.data.frame(BRCA_dat))
-BRCA_cts <- BRCA_cts[!duplicated(BRCA_cts[,1:ncol(BRCA_cts)]),idy]
-
-# DESeq2 to find top significant genes in clustering
-# colnames(BRCA_cts)<-toupper(colnames(BRCA_cts))
-# coldata<-as.matrix(BRCA_subtypes[idy])
-# rownames(coldata)<-colnames(BRCA_cts)
-# colnames(coldata)<-"subtype"
-# #all(rownames(coldata) %in% colnames(cts))         # check that headers are correct
-# #all(rownames(coldata) == colnames(cts))
-# dds<-DESeqDataSetFromMatrix(countData = BRCA_cts,
-#                             colData = coldata,
-#                             design = ~ subtype)
-# DESeq_dds<-DESeq(dds)
-# 
-# res<-results(DESeq_dds,alpha=0.05)
-# signif_res<-res[is.na(res$padj)==FALSE,]
-# signif_res<-signif_res[order(signif_res$padj),]
-# signif_res<-signif_res[1:2500,]
-# 
-# BRCA_dat <- BRCA_cts[rownames(signif_res),]
-
-
-
-
-# NSCLC first 2000 genes run #
+## Using Row MAD
 #row_mad <- apply(NSCLC_dat,1,mad)
 #NSCLC_dat <- NSCLC_dat[order(-row_mad),]
+
+
+# RUN
 y1 <- NSCLC_dat
 fit <- normalizations(y1)
 size_factors <- fit$size_factors
@@ -230,36 +195,99 @@ X1<-compare(y1)
 
 
 
-# BRCA first 2000 genes, 100 samples run #
+
+
+
+################################ BRCA (Breast Cancer) Data #######################################
+
+library(SummarizedExperiment)
+library(genefilter)
+
+# DATA PREP
+
+load(file="TCGA_BRCA_exp.rda")
+BRCA_cts <- round(assay(data),digits=0)             # default gives raw count
+BRCA_anno <- colData(data)@listData
+BRCA_subtypes <- BRCA_anno$subtype_PAM50.mRNA
+
+idy <- (!is.na(BRCA_subtypes) & BRCA_subtypes != "Normal-like")
+BRCA_cts <- BRCA_cts[!duplicated(BRCA_cts[,1:ncol(BRCA_cts)]),idy]
 
 BRCA_dat <- BRCA_cts
 
+
+# PRE-FILTERING
+
+## Row MAD
 row_mad <- apply(BRCA_dat,1,mad)
 BRCA_dat <- BRCA_dat[order(-row_mad),]
 
+## Row Variances
 #row_var <- rowVars(BRCA_cts)
 #BRCA_cts <- BRCA_cts[order(-row_var),]
 
-y2 <- BRCA_dat[1:100,]
+
+# RUN
+
+y2 <- BRCA_dat[1:1000,]
 fit <- normalizations(y2)
 size_factors <- fit$size_factors
 norm_y <- fit$norm_y
 
 y2 <- y2[rowSums(y2)>=100,]
 
-X2 <- compare(y2)
+# X2 <- compare(y2)
 
 
 
+load("X1.out")
+load("X2.out")
 
+rowsum1=apply(abs(X1$i_X$beta[[1]]),1,sum)
+cutoff1=(X1$EM_X$lambda2)
+features1=rownames(y1)
+isigfeatures1=features1[which(rowsum1>cutoff1)]
 
-y3<-  BRCA_cts[1:15000,1:50]
-fit<-normalizations(y3)
-size_factors<-fit$size_factors
-norm_y<-fit$norm_y
-y3<-y3[rowSums(y3)>=100,]
-X3<-compare(y3)
+rowsum2=apply(abs(X2$i_X$beta[[1]]),1, sum)
+cutoff2=X2$EM_X$lambda2
+features2=rownames(y2)
+isigfeatures2=(features2)[which(rowsum2>cutoff2)]
 
+write_csv = function(X,y){
+  icoefs = cbind(X$i_X$alpha[[1]], X$i_X$alpha[[1]] + X$i_X$beta[[1]])
+  EMcoefs = X$EM_X$coefs
+  
+  i_FC = (apply(icoefs,1,max) - apply(icoefs,1,min))/(X$i_maxK - 1)      # avg FC = (Max - min) / (#clusters - 1)
+  EM_FC = (apply(EMcoefs,1,max) - apply(EMcoefs,1,min))/(X$EM_maxK - 1)
+  
+  lambda2 = X$EM_X$lambda2
+  features=rownames(y)
+  
+  isig_id=which(i_FC>lambda2)
+  EMsig_id1=(X$EM_X$nondiscriminatory==FALSE) # EM theta = 0 for all K as criteria
+  EMsig_id2=which(EM_FC>lambda2)       # EM FC > lambda2 as criteria for discriminatory
+                                             # These criteria are identical for K=2, but not in general
+  isig_feat=features[isig_id]
+  EMsig_feat1=features[EMsig_id1]
+  EMsig_feat2=features[EMsig_id2]
+  
+  
+  # Order by FC
+  i_order = order(-i_FC)
+  EM_order = order(-EM_FC)
+  
+  tab <- cbind(features[i_order],i_FC[i_order],(features[i_order] %in% isig_feat)^2,
+               features[EM_order],EM_FC[EM_order],(features[EM_order] %in% EMsig_feat1)^2,(features[EM_order] %in% EMsig_feat2)^2)
+  colnames(tab) <- c("i_Features","i_FC","i_sig","EM_Features","EM_FC","EM_sig_theta","EM_sig_FC")
+
+  return(tab)
+}
+
+tab1<-write_csv(X1,y1)
+tab2<-write_csv(X2,y2)
+
+write.csv(tab1,"NSCLC_comparison.csv")
+write.csv(tab2,"BRCA_comparison.csv")
 
 
 ################################################
